2774f0daade9162be066200242fd3273e7661ea9
Crispín Avís <crispin.avis.gomez@gmail.com>
2017-01-17 19:14:23 +0100

    Algunos ejercicios atrasados

---
+++ Crispin/publico/Ejercicios_clase-06/Ejercicio-1.js
@@ -0,0 +1,11 @@
+/* 1 - Diseña un script que simula el lanzamiento de una moneda al aire e imprimir si ha salido cara o cruz.
+*/
+
+
+var valor = Math.floor(Math.random() * (2 - 0)) + 0
+
+if (valor===1){
+    console.log("Cara");
+    } else{
+       console.log("Cruz"); 
+}
