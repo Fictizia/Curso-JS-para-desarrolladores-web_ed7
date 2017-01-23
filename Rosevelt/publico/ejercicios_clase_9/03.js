05a97bf7a8dd1bb132dcae722270e6c1d695ace0
Rosevelt Barahona <rosevelt@iMac-de-rosevelt.local>
2017-01-24 00:43:08 +0100

    actualizando ejercicios

--- Rosevelt/publico/ejercicios_clase_9/03.js
+++ Rosevelt/publico/ejercicios_clase_9/03.js
@@ -7,9 +7,8 @@ Cada asistente recibirá el identificador de su posición en el Array.
 El identificador no puede cambiar una vez reservado el asiento.
 Los asistentes pueden darse de baja, y el asiento que ocupaban queda libre.
 Los nuevos asistentes seran asigandos primero a asientos desocupados.
-Opcional:
 
-Verifica antes de asignar un asistente que no se registro previamente. */
+Opcional: Verifica antes de asignar un asistente que no se registro previamente. */
 
  var invitados = ['Jose' , 'Nacho' , 'Mario' , 'Hector', 'Helena','David'];
 
@@ -24,24 +23,23 @@ Verifica antes de asignar un asistente que no se registro previamente. */
 	darseDeBaja ('Nacho');
 
 
-
-
-
 	function nuevo(nuevoInvitado){
 
-		function siAlguno (elemento){
-			return elemento === undefined;
-		}
-
-		//nuevoInvitado = nuevoInvitado.toUpperCase();
+		
 		if(invitados.indexOf(nuevoInvitado) >=0 ){
 			console.info("lo siento ya esta registrado" , "tu asiento es el:" , invitados.indexOf(nuevoInvitado));
 		}
 
 		/*usando some
+
+		function siAlguno (elemento){
+			return elemento === undefined;
+		}
+
 		else if ( invitados.some(siAlguno) ){
 			invitados[invitados.indexOf(undefined)] = nuevoInvitado;
-		}*/
+		}
+		*/
 
 		else if ( invitados.indexOf(undefined) > 0 ){
 			invitados[invitados.indexOf(undefined)] = nuevoInvitado;
--- Rosevelt/publico/ejercicios_clase_9/03_v2.js
+++ Rosevelt/publico/ejercicios_clase_9/03_v2.js
@@ -1,29 +1,28 @@
 
-/*
- 3 - Vamos a gestionar la lista de asistentes de un evento.
+/* 3 - Vamos a gestionar la lista de asistentes de un evento.
 
 Características:
 Cada asistente recibirá el identificador de su posición en el Array.
 El identificador no puede cambiar una vez reservado el asiento.
 Los asistentes pueden darse de baja, y el asiento que ocupaban queda libre.
 Los nuevos asistentes seran asigandos primero a asientos desocupados.
-Opcional:
-
-Verifica antes de asignar un asistente que no se registro previamente. */
+Opcional:  Verifica antes de asignar un asistente que no se registro previamente. */
 
 
  var invitados = [];
 
 	function nuevo(nuevoInvitado){
 
+		// valido si el nombre ya esta en el array y le informo que ya esta registrado y su "asiento"
 		if(invitados.indexOf(nuevoInvitado) >=0 ){
 			console.info('lo siento ' + nuevoInvitado + ' ya esta registrado' , 'tu asiento es el:' , invitados.indexOf(nuevoInvitado));
 		}
 
+		// si hay algun elemento como undefined en el array lo capturo y le asigno al nuevo invitado esa posición
 		else if ( invitados.indexOf(undefined) > 0 ){
 			invitados[invitados.indexOf(undefined)] = nuevoInvitado;
 		}
-
+		// si no simplemente lo añado al final del array
 		else{
 				
 			invitados.push(nuevoInvitado);
@@ -39,23 +38,25 @@ Verifica antes de asignar un asistente que no se registro previamente. */
 
 
 	function darseDeBaja (nombre){
+		// valido si esta en la lista de invitados
 	 	if(invitados.indexOf(nombre) > 0){
-		 	//capturo su posición en el array
 		 	eliminar = invitados.indexOf(nombre);
-		 	// lo elimina pasandolo a undefined , nadie pierde su indice
+		 	// lo elimino usando delete para que esa posición se quede como indefined , nadie pierde su indice.
 		 	delete invitados[eliminar];
-		 	invitados[eliminar] = undefined;
+		 	// fuerzo el undefined por que me daba error en la consola ¿? // podria sobreescribirlo a otro valor x ejm "asiento vacio"
+		 	invitados[eliminar] = undefined; 
 		 }else{
 		 	console.info('no te peudes dar de baja ' + nombre + ' no estas en la lista');
 		 }
 	 }
 
+
 	darseDeBaja ('nuria');
 	darseDeBaja ('carla');
 	console.info(invitados);
+
 	nuevo('rosevelt');
 	nuevo('Ana');
-
 	console.info(invitados);
 
 
